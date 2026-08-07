ae75377a64161d6ff5734a02a1bdb308edf8060a
Pim Pieters <pim@pieters.tv>
2026-08-08 01:56:49 +0200

    Add engine execution support

--- glibness/engine.go
+++ glibness/engine.go
@@ -5,30 +5,76 @@ import (
 )
 
 type Engine struct {
-	dialogues map[string]Dialogue
+	Dialogues []Dialogue
 }
 
-type DialogueExecution struct {
-	engine    *Engine
-	speaker   string
-	sentence  string
-	variables map[string]string
+type DialogueState struct {
+	Engine   *Engine
+	Dialogue *Dialogue
+	Index    int
+
+	Speaker   string
+	Sentence  string
+	Variables map[string]string
+	Finished  bool
 }
 
-func (e *DialogueExecution) execute(s Statement) error {
+const (
+	DialogueChangeError    = iota
+	DialogueChangeStop     = iota
+	DialogueChangeSay      = iota
+	DialogueChangeInternal = iota
+)
+
+func (engine *Engine) Start(name string) (DialogueState, error) {
+	for _, dialogue := range engine.Dialogues {
+		if dialogue.name == name {
+			return DialogueState{
+				Engine:    engine,
+				Dialogue:  &dialogue,
+				Index:     0,
+				Speaker:   "",
+				Sentence:  "",
+				Variables: make(map[string]string),
+				Finished:  false,
+			}, nil
+		}
+	}
+
+	return DialogueState{}, fmt.Errorf("Could not find dialogue with name '%s'", name)
+}
+
+func (state *DialogueState) execute(s Statement) (int, error) {
 	if say, ok := s.(SayStatement); ok {
-		e.sentence = say.sentence
-		return nil
+		state.Sentence = say.sentence
+		return DialogueChangeSay, nil
 	}
 
 	if set, ok := s.(SetStatement); ok {
 		if set.variable == "speaker" {
-			e.speaker = set.value
+			state.Speaker = set.value
 		} else {
-			e.variables[set.variable] = set.value
+			state.Variables[set.variable] = set.value
 		}
-		return nil
+		return DialogueChangeInternal, nil
 	}
 
-	return fmt.Errorf("Unsupported statement: %s", s.String())
+	return DialogueChangeError, fmt.Errorf("Unsupported statement: %s", s.String())
+}
+
+func (state *DialogueState) Next() (int, error) {
+	if state.Finished {
+		return DialogueChangeStop, nil
+	}
+
+	if state.Index >= len(state.Dialogue.statements) {
+		state.Finished = true
+		return DialogueChangeStop, nil
+	}
+
+	statement := state.Dialogue.statements[state.Index]
+	status, err := state.execute(statement)
+	state.Index += 1
+
+	return status, err
 }
