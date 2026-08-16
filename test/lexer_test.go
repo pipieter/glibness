feaff6af71ffb045c3530453ee56bca7a23c73b2
Pim Pieters <pim@pieters.tv>
2026-08-16 20:10:02 +0200

    Fix trailing whitespaces in glib files

---
+++ test/lexer_test.go
@@ -0,0 +1,21 @@
+package test
+
+import (
+	"glibness/glibness"
+	"testing"
+
+	assert "github.com/stretchr/testify/assert"
+)
+
+func TestTrailingWhitespace(t *testing.T) {
+	assert := assert.New(t)
+	engine := glibness.NewEngine()
+
+	err := engine.ParseString(`
+		dialogue test {
+			say "This is a message with trailing whitespaces."     
+			say "This message also has trailing whitespaces." 		    
+		}
+	`)
+	assert.Nil(err)
+}
